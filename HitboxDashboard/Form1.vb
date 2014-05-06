539d7681b4a88dfd0a41309a6aa7762dc842376b
Arkkis <j@arkkis.net>
2014-05-06 03:35:54 +0300

    Visual Studio 2013 Project
    Visual Studio 2013 Project

---
+++ HitboxDashboard/Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Net
+
+Public Class Form1
+
+    Dim cdn As String = DownloadString("https://docs.google.com/document/d/1m41TZWmNuKDwuYOHA2_ZX9J9Gd6gGTTIfJV6DHcjlpI/edit?usp=sharing")
+
+    Function DownloadString(ByVal address As String)
+        Dim client As WebClient = New WebClient()
+        Dim reply As String = client.DownloadString(address)
+
+        Return reply
+    End Function
+End Class
