2e977f779b22bfe7a5d972d5f007e12f231aca08
Arkkis <j@arkkis.net>
2014-05-06 05:01:43 +0300

    Added simple data labels
    Pulling the data from Hitbox API to labels

--- HitboxDashboard/Form1.Designer.vb
+++ HitboxDashboard/Form1.Designer.vb
@@ -22,17 +22,95 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.DataLabel = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.TitleLabel = New System.Windows.Forms.Label()
+        Me.GameLabel = New System.Windows.Forms.Label()
+        Me.FollowersLabel = New System.Windows.Forms.Label()
+        Me.ViewersLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
+        '
+        'DataLabel
+        '
+        Me.DataLabel.AutoSize = True
+        Me.DataLabel.Location = New System.Drawing.Point(13, 13)
+        Me.DataLabel.Name = "DataLabel"
+        Me.DataLabel.Size = New System.Drawing.Size(30, 13)
+        Me.DataLabel.TabIndex = 0
+        Me.DataLabel.Text = "Data"
+        '
+        'BackgroundWorker1
+        '
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.AutoSize = True
+        Me.StatusLabel.Location = New System.Drawing.Point(13, 26)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(37, 13)
+        Me.StatusLabel.TabIndex = 1
+        Me.StatusLabel.Text = "Status"
+        '
+        'TitleLabel
+        '
+        Me.TitleLabel.AutoSize = True
+        Me.TitleLabel.Location = New System.Drawing.Point(13, 39)
+        Me.TitleLabel.Name = "TitleLabel"
+        Me.TitleLabel.Size = New System.Drawing.Size(27, 13)
+        Me.TitleLabel.TabIndex = 2
+        Me.TitleLabel.Text = "Title"
+        '
+        'GameLabel
+        '
+        Me.GameLabel.AutoSize = True
+        Me.GameLabel.Location = New System.Drawing.Point(13, 52)
+        Me.GameLabel.Name = "GameLabel"
+        Me.GameLabel.Size = New System.Drawing.Size(35, 13)
+        Me.GameLabel.TabIndex = 3
+        Me.GameLabel.Text = "Game"
+        '
+        'FollowersLabel
+        '
+        Me.FollowersLabel.AutoSize = True
+        Me.FollowersLabel.Location = New System.Drawing.Point(13, 65)
+        Me.FollowersLabel.Name = "FollowersLabel"
+        Me.FollowersLabel.Size = New System.Drawing.Size(51, 13)
+        Me.FollowersLabel.TabIndex = 4
+        Me.FollowersLabel.Text = "Followers"
+        '
+        'ViewersLabel
+        '
+        Me.ViewersLabel.AutoSize = True
+        Me.ViewersLabel.Location = New System.Drawing.Point(13, 78)
+        Me.ViewersLabel.Name = "ViewersLabel"
+        Me.ViewersLabel.Size = New System.Drawing.Size(44, 13)
+        Me.ViewersLabel.TabIndex = 5
+        Me.ViewersLabel.Text = "Viewers"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(289, 253)
+        Me.Controls.Add(Me.ViewersLabel)
+        Me.Controls.Add(Me.FollowersLabel)
+        Me.Controls.Add(Me.GameLabel)
+        Me.Controls.Add(Me.TitleLabel)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.DataLabel)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents DataLabel As System.Windows.Forms.Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents StatusLabel As System.Windows.Forms.Label
+    Friend WithEvents TitleLabel As System.Windows.Forms.Label
+    Friend WithEvents GameLabel As System.Windows.Forms.Label
+    Friend WithEvents FollowersLabel As System.Windows.Forms.Label
+    Friend WithEvents ViewersLabel As System.Windows.Forms.Label
 
 End Class
--- HitboxDashboard/Form1.vb
+++ HitboxDashboard/Form1.vb
@@ -1,8 +1,31 @@
-﻿Imports System.Net
+﻿Imports System
+Imports System.Net
+Imports System.Threading
 
 Public Class Form1
 
-    Dim cdn As String = DownloadString("https://docs.google.com/document/d/1m41TZWmNuKDwuYOHA2_ZX9J9Gd6gGTTIfJV6DHcjlpI/edit?usp=sharing")
+    Dim dburl As String = DownloadString("https://googledrive.com/host/0BwXzp8oa9Tx4eU93R0xUNkFHa00/dashboard.txt")
+    Dim data, status, title, game, followers, viewers As String
+    Dim dataArray As Array
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        data = DownloadString(dburl + "?needed")
+        UpdateUI(data)
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        data = DownloadString(dburl + "?needed")
+        e.Result = data
+        Thread.Sleep(5000)
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        data = e.Result
+        DataLabel.Text = e.Result
+        UpdateUI(data)
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
 
     Function DownloadString(ByVal address As String)
         Dim client As WebClient = New WebClient()
@@ -10,4 +33,19 @@ Public Class Form1
 
         Return reply
     End Function
+
+    Function UpdateUI(data As String)
+        dataArray = Split(data, "||")
+        status = dataArray(0)
+        title = dataArray(1)
+        game = dataArray(2)
+        followers = dataArray(3)
+        viewers = dataArray(4)
+        StatusLabel.Text = status
+        TitleLabel.Text = title
+        GameLabel.Text = game
+        FollowersLabel.Text = followers
+        ViewersLabel.Text = viewers
+    End Function
+
 End Class
