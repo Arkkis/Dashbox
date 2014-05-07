11bd725b3a377921f90b1d350bdb4fee9acc9381
Arkkis <j@arkkis.net>
2014-05-07 23:21:01 +0300

    Major additions
    IRC to handle updates through Glados,
    More complete UI,
    Autocomplete in Game-field.

--- HitboxDashboard/Form1.Designer.vb
+++ HitboxDashboard/Form1.Designer.vb
@@ -22,23 +22,21 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataLabel = New System.Windows.Forms.Label()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.StatusLabel = New System.Windows.Forms.Label()
-        Me.TitleLabel = New System.Windows.Forms.Label()
-        Me.GameLabel = New System.Windows.Forms.Label()
         Me.FollowersLabel = New System.Windows.Forms.Label()
         Me.ViewersLabel = New System.Windows.Forms.Label()
+        Me.PasswordTextBox = New System.Windows.Forms.TextBox()
+        Me.NickTextBox = New System.Windows.Forms.TextBox()
+        Me.GameTextBox = New System.Windows.Forms.TextBox()
+        Me.TitleTextBox = New System.Windows.Forms.TextBox()
+        Me.SetTitleLabel = New System.Windows.Forms.Label()
+        Me.SetGameLabel = New System.Windows.Forms.Label()
+        Me.SetNickLabel = New System.Windows.Forms.Label()
+        Me.SetPasswordLabel = New System.Windows.Forms.Label()
+        Me.UpdateDataButton = New System.Windows.Forms.Button()
         Me.SuspendLayout()
-        '
-        'DataLabel
-        '
-        Me.DataLabel.AutoSize = True
-        Me.DataLabel.Location = New System.Drawing.Point(13, 13)
-        Me.DataLabel.Name = "DataLabel"
-        Me.DataLabel.Size = New System.Drawing.Size(30, 13)
-        Me.DataLabel.TabIndex = 0
-        Me.DataLabel.Text = "Data"
         '
         'BackgroundWorker1
         '
@@ -46,71 +44,146 @@ Partial Class Form1
         'StatusLabel
         '
         Me.StatusLabel.AutoSize = True
-        Me.StatusLabel.Location = New System.Drawing.Point(13, 26)
+        Me.StatusLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.StatusLabel.ForeColor = System.Drawing.Color.Green
+        Me.StatusLabel.Location = New System.Drawing.Point(12, 9)
         Me.StatusLabel.Name = "StatusLabel"
-        Me.StatusLabel.Size = New System.Drawing.Size(37, 13)
+        Me.StatusLabel.Size = New System.Drawing.Size(66, 24)
         Me.StatusLabel.TabIndex = 1
         Me.StatusLabel.Text = "Status"
-        '
-        'TitleLabel
-        '
-        Me.TitleLabel.AutoSize = True
-        Me.TitleLabel.Location = New System.Drawing.Point(13, 39)
-        Me.TitleLabel.Name = "TitleLabel"
-        Me.TitleLabel.Size = New System.Drawing.Size(27, 13)
-        Me.TitleLabel.TabIndex = 2
-        Me.TitleLabel.Text = "Title"
-        '
-        'GameLabel
-        '
-        Me.GameLabel.AutoSize = True
-        Me.GameLabel.Location = New System.Drawing.Point(13, 52)
-        Me.GameLabel.Name = "GameLabel"
-        Me.GameLabel.Size = New System.Drawing.Size(35, 13)
-        Me.GameLabel.TabIndex = 3
-        Me.GameLabel.Text = "Game"
         '
         'FollowersLabel
         '
         Me.FollowersLabel.AutoSize = True
-        Me.FollowersLabel.Location = New System.Drawing.Point(13, 65)
+        Me.FollowersLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FollowersLabel.Location = New System.Drawing.Point(142, 39)
         Me.FollowersLabel.Name = "FollowersLabel"
-        Me.FollowersLabel.Size = New System.Drawing.Size(51, 13)
+        Me.FollowersLabel.Size = New System.Drawing.Size(85, 20)
         Me.FollowersLabel.TabIndex = 4
         Me.FollowersLabel.Text = "Followers"
         '
         'ViewersLabel
         '
         Me.ViewersLabel.AutoSize = True
-        Me.ViewersLabel.Location = New System.Drawing.Point(13, 78)
+        Me.ViewersLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ViewersLabel.Location = New System.Drawing.Point(12, 39)
         Me.ViewersLabel.Name = "ViewersLabel"
-        Me.ViewersLabel.Size = New System.Drawing.Size(44, 13)
+        Me.ViewersLabel.Size = New System.Drawing.Size(72, 20)
         Me.ViewersLabel.TabIndex = 5
         Me.ViewersLabel.Text = "Viewers"
+        '
+        'PasswordTextBox
+        '
+        Me.PasswordTextBox.Location = New System.Drawing.Point(72, 152)
+        Me.PasswordTextBox.Name = "PasswordTextBox"
+        Me.PasswordTextBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.PasswordTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.PasswordTextBox.TabIndex = 6
+        '
+        'NickTextBox
+        '
+        Me.NickTextBox.Location = New System.Drawing.Point(72, 126)
+        Me.NickTextBox.Name = "NickTextBox"
+        Me.NickTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.NickTextBox.TabIndex = 7
+        '
+        'GameTextBox
+        '
+        Me.GameTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Append
+        Me.GameTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource
+        Me.GameTextBox.Location = New System.Drawing.Point(72, 100)
+        Me.GameTextBox.Name = "GameTextBox"
+        Me.GameTextBox.Size = New System.Drawing.Size(205, 20)
+        Me.GameTextBox.TabIndex = 8
+        '
+        'TitleTextBox
+        '
+        Me.TitleTextBox.Location = New System.Drawing.Point(72, 74)
+        Me.TitleTextBox.Name = "TitleTextBox"
+        Me.TitleTextBox.Size = New System.Drawing.Size(205, 20)
+        Me.TitleTextBox.TabIndex = 9
+        '
+        'SetTitleLabel
+        '
+        Me.SetTitleLabel.AutoSize = True
+        Me.SetTitleLabel.Location = New System.Drawing.Point(12, 77)
+        Me.SetTitleLabel.Name = "SetTitleLabel"
+        Me.SetTitleLabel.Size = New System.Drawing.Size(27, 13)
+        Me.SetTitleLabel.TabIndex = 10
+        Me.SetTitleLabel.Text = "Title"
+        '
+        'SetGameLabel
+        '
+        Me.SetGameLabel.AutoSize = True
+        Me.SetGameLabel.Location = New System.Drawing.Point(13, 103)
+        Me.SetGameLabel.Name = "SetGameLabel"
+        Me.SetGameLabel.Size = New System.Drawing.Size(35, 13)
+        Me.SetGameLabel.TabIndex = 11
+        Me.SetGameLabel.Text = "Game"
+        '
+        'SetNickLabel
+        '
+        Me.SetNickLabel.AutoSize = True
+        Me.SetNickLabel.Location = New System.Drawing.Point(13, 129)
+        Me.SetNickLabel.Name = "SetNickLabel"
+        Me.SetNickLabel.Size = New System.Drawing.Size(55, 13)
+        Me.SetNickLabel.TabIndex = 12
+        Me.SetNickLabel.Text = "Nickname"
+        '
+        'SetPasswordLabel
+        '
+        Me.SetPasswordLabel.AutoSize = True
+        Me.SetPasswordLabel.Location = New System.Drawing.Point(13, 155)
+        Me.SetPasswordLabel.Name = "SetPasswordLabel"
+        Me.SetPasswordLabel.Size = New System.Drawing.Size(53, 13)
+        Me.SetPasswordLabel.TabIndex = 13
+        Me.SetPasswordLabel.Text = "Password"
+        '
+        'UpdateDataButton
+        '
+        Me.UpdateDataButton.Location = New System.Drawing.Point(178, 126)
+        Me.UpdateDataButton.Name = "UpdateDataButton"
+        Me.UpdateDataButton.Size = New System.Drawing.Size(99, 46)
+        Me.UpdateDataButton.TabIndex = 14
+        Me.UpdateDataButton.Text = "Update"
+        Me.UpdateDataButton.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(289, 253)
+        Me.ClientSize = New System.Drawing.Size(289, 181)
+        Me.Controls.Add(Me.UpdateDataButton)
+        Me.Controls.Add(Me.SetPasswordLabel)
+        Me.Controls.Add(Me.SetNickLabel)
+        Me.Controls.Add(Me.SetGameLabel)
+        Me.Controls.Add(Me.SetTitleLabel)
+        Me.Controls.Add(Me.TitleTextBox)
+        Me.Controls.Add(Me.GameTextBox)
+        Me.Controls.Add(Me.NickTextBox)
+        Me.Controls.Add(Me.PasswordTextBox)
         Me.Controls.Add(Me.ViewersLabel)
         Me.Controls.Add(Me.FollowersLabel)
-        Me.Controls.Add(Me.GameLabel)
-        Me.Controls.Add(Me.TitleLabel)
         Me.Controls.Add(Me.StatusLabel)
-        Me.Controls.Add(Me.DataLabel)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Dashbox"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents DataLabel As System.Windows.Forms.Label
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
     Friend WithEvents StatusLabel As System.Windows.Forms.Label
-    Friend WithEvents TitleLabel As System.Windows.Forms.Label
-    Friend WithEvents GameLabel As System.Windows.Forms.Label
     Friend WithEvents FollowersLabel As System.Windows.Forms.Label
     Friend WithEvents ViewersLabel As System.Windows.Forms.Label
+    Friend WithEvents PasswordTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents NickTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents GameTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents TitleTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents SetTitleLabel As System.Windows.Forms.Label
+    Friend WithEvents SetGameLabel As System.Windows.Forms.Label
+    Friend WithEvents SetNickLabel As System.Windows.Forms.Label
+    Friend WithEvents SetPasswordLabel As System.Windows.Forms.Label
+    Friend WithEvents UpdateDataButton As System.Windows.Forms.Button
 
 End Class
--- HitboxDashboard/Form1.vb
+++ HitboxDashboard/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System
+Imports System.IO
 Imports System.Net
 Imports System.Threading
 
@@ -8,7 +9,29 @@ Public Class Form1
     Dim data, status, title, game, followers, viewers As String
     Dim dataArray As Array
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Dim port As Integer, connectionclose As Integer = 0
+    Dim buf As String, login, nick As String, pass As String, owner As String, server As String, chan As String, settitle As String, setgame As String
+    Dim input As TextReader
+    Dim output As TextWriter
+
+    Dim sr As StreamReader = Nothing
+    Dim MyContents As New AutoCompleteStringCollection
+
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If File.Exists(Application.StartupPath & "\db.txt") Then
+            sr = File.OpenText(Application.StartupPath & "\db.txt")
+
+            While Not sr.EndOfStream
+                MyContents.Add(sr.ReadLine)
+            End While
+
+            sr.Close()
+        End If
+
+        GameTextBox.AutoCompleteCustomSource = MyContents
+        GameTextBox.AutoCompleteSource = AutoCompleteSource.CustomSource
+        GameTextBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend
         data = DownloadString(dburl + "?needed")
         UpdateUI(data)
         BackgroundWorker1.RunWorkerAsync()
@@ -22,7 +45,6 @@ Public Class Form1
 
     Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
         data = e.Result
-        DataLabel.Text = e.Result
         UpdateUI(data)
         BackgroundWorker1.RunWorkerAsync()
     End Sub
@@ -41,11 +63,83 @@ Public Class Form1
         game = dataArray(2)
         followers = dataArray(3)
         viewers = dataArray(4)
-        StatusLabel.Text = status
-        TitleLabel.Text = title
-        GameLabel.Text = game
-        FollowersLabel.Text = followers
-        ViewersLabel.Text = viewers
+        TitleTextBox.Text = title
+        GameTextBox.Text = game
+        FollowersLabel.Text = "Followers: " & followers
+        ViewersLabel.Text = "Viewers: " & viewers
+
+        If status = 1 Then
+            StatusLabel.Text = "Online"
+        Else
+            StatusLabel.Text = "Offline"
+        End If
+
     End Function
 
+    Function UpdateData()
+        Dim sock As New System.Net.Sockets.TcpClient()
+
+        nick = NickTextBox.Text
+        pass = PasswordTextBox.Text
+        owner = NickTextBox.Text
+        server = "irc.glados.tv"
+        port = Convert.ToInt32("6667")
+        chan = "#pelilegacy"
+
+        settitle = TitleTextBox.Text
+        setgame = GameTextBox.Text
+
+        sock.Connect(server, port)
+        If Not sock.Connected Then
+            Debug.WriteLine("Failed to connect!")
+        Else
+            input = New System.IO.StreamReader(sock.GetStream())
+            output = New System.IO.StreamWriter(sock.GetStream())
+
+            'Starting USER and NICK login commands 
+            'output.Write("PASS " & pass & vbCr & vbLf & "USER " & nick & " 0 * :" & owner & vbCr & vbLf & "NICK " & nick & vbCr & vbLf)
+            login = "PASS " & pass & vbCr & vbLf & "NICK " & nick & vbCr & vbLf & "USER " & nick & " 0 * :" & owner & vbCr & vbLf
+            Debug.WriteLine(login)
+            output.Write(login)
+            output.Flush()
+
+            buf = input.ReadLine()
+            'lastbuf = ""
+            'log = ""
+            While connectionclose = 0
+
+                If buf.StartsWith("PING ") Then
+                    output.Write(buf.Replace("PING", "PONG") & vbCr & vbLf)
+                    output.Flush()
+                End If
+                If buf(0) <> ":"c Then
+                    Continue While
+                End If
+
+                If buf.Split(" "c)(1) = "001" Then
+                    output.Write("MODE " & nick & " +B" & vbCr & vbLf & "JOIN " & chan & vbCr & vbLf)
+                    output.Flush()
+                End If
+
+                If buf.Split(" "c)(1) = "366" Then
+                    output.Write("PRIVMSG #pelilegacy :!title " & settitle & vbCr & vbLf)
+                    output.Flush()
+                    output.Write("PRIVMSG #pelilegacy :!game " & setgame & vbCr & vbLf)
+                    output.Flush()
+                    output.Write("QUIT :Disconnected" & vbCr & vbLf)
+                    output.Flush()
+                    System.Threading.Thread.Sleep(3000)
+                    connectionclose = 1
+                End If
+                buf = input.ReadLine()
+                If connectionclose = 1 Then
+                    sock.Close()
+                End If
+            End While
+        End If
+    End Function
+
+    Private Sub UpdateDataButton_Click(sender As Object, e As EventArgs) Handles UpdateDataButton.Click
+        UpdateData()
+    End Sub
 End Class
